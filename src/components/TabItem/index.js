0b254d513e22804febb7c769014c6e042817f8ab
Madhu <mulaglam@gmail.com>
2022-04-20 07:14:27 +0000

    projects Application using react

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, updateActiveTabId, isActive} = props
+  const {tabId, displayText} = tabDetails
+
+  const onClickUpdateButton = () => {
+    updateActiveTabId(tabId)
+  }
+
+  const activeTabClassName = isActive ? 'active-tab-btn' : ''
+
+  return (
+    <li className="tab-item-container ">
+      <button
+        type="button"
+        className={`tab-btn ${activeTabClassName}`}
+        onClick={onClickUpdateButton}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
